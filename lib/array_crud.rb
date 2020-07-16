a9cd2c056c4fc6e5c0a9cb0cb0294273859d1a47
Cory Marker <cmarker4@gmail.com>
2020-07-16 16:06:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift(element)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -19,17 +19,17 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_empty_array.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array[0]
 end
 
 def retrieve_first_element_from_array(array)
-
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  array.last
 end
